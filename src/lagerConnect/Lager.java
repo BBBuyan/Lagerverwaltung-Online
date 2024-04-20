53100abafcfadcd7b4667d39d7ee2a0ad41f6cd3
BBBuyan <bbuyan5@gmail.com>
2024-04-20 10:09:10 +0200

    sql Verbindung aufgebaut Bato eingefügt

--- src/lagerConnect/Lager.java
+++ src/lagerConnect/Lager.java
@@ -1,5 +1,10 @@
 package lagerConnect;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
+
 public class Lager {
 	
 	private LagerData data;
@@ -21,6 +26,55 @@ public class Lager {
 		a.getFachat(1, 1).addWare(test);
 		a.getFachat(1, 1).addWare(test2);
 		System.out.println(a.getFachat(1, 1).warenList()+a.getFachat(1, 1).getFreiPlatz());
+		
+		
+		//SQL Connection Bereich
+		Connection conn = null;
+		try {
+			
+			String url = "jdbc:oracle:thin:@141.57.9.192:1521:imora19c";
+			String username = "C##GDB23013";
+			String pass = "oracle";
+			
+			Statement smt = null;
+			
+			
+			DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());	
+			conn = DriverManager.getConnection(url,username,pass);
+			
+			if(conn != null)
+			{
+				System.out.println("Connected!!!");
+				smt = conn.createStatement();
+				String sql = "insert into testing(id, name, age) values (3,'Bato', 21)";
+				smt.execute(sql);
+				System.out.println("sql executed");
+			}
+			else
+			{
+				System.out.println("Failed to Connect!!!");
+			}
+			
+		} 
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		} 
+		finally 
+		{
+			
+			try {
+				if(conn !=null)
+				{
+					conn.close();
+				}
+			} catch (SQLException e)
+			{
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
 	}
-
 }
